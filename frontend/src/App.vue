b993ce54d86f698722265807867f48acdb025ebc
Axel <axelcayuela@hotmail.com>
2020-05-28 17:02:31 +0200

    Axel: Upload wails sudoku project to github repository

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div class="start">
+      <img alt="Wails logo" src="./assets/images/sudoku_logo.png" class="logo zoomIn">
+          <br><br>
+          <button class="buttonStart">Start Game</button> 
+    </div>
+    
+  </div>
+</template>
+
+<script>
+
+import "./assets/css/main.css";
+
+export default {
+  name: "app",
+ 
+};
+</script>
