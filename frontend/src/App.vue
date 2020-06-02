4a5bf7518df9dfcdbe311ec7ce96f45ec65bed1b
Axel <axelcayuela@hotmail.com>
2020-06-02 11:53:46 +0200

    Axel: sudoku table frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,84 @@
 <template>
   <div id="app">
     <div class="start">
-      <img alt="Wails logo" src="./assets/images/sudoku_logo.png" class="logo zoomIn">
+      <!-- <img alt="Wails logo" src="./assets/images/sudoku_logo.png" class="logo zoomIn"> -->
           <br><br>
+            <table>
+              <tr> 
+              <td><input type="number" name="" value="1"></td> 
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td> 
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td> 
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td>
+            </tr>
+            <tr>
+              <td><input type="number" name="" value="1"></td> 
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td> 
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td> 
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td>
+
+            </tr>
+              <tr>
+                <td><input type="number" name="" value="1"></td> 
+                <td><input type="number" name="" value="1"></td>
+                <td><input type="number" name="" value="1"></td>
+                <td><input type="number" name="" value="1"></td> 
+                <td><input type="number" name="" value="1"></td>
+                <td><input type="number" name="" value="1"></td>
+                <td><input type="number" name="" value="1"></td> 
+                <td><input type="number" name="" value="1"></td>
+                <td><input type="number" name="" value="1"></td>
+              </tr>
+                 <tr> 
+              <td><input type="number" name="" value="1"></td> 
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td> 
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td> 
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td>
+            </tr>
+            <tr>
+              <td><input type="number" name="" value="1"></td> 
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td> 
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td> 
+              <td><input type="number" name="" value="1"></td>
+              <td><input type="number" name="" value="1"></td>
+
+            </tr>
+              <tr>
+                <td><input type="number" name="" value="1"></td> 
+                <td><input type="number" name="" value="1"></td>
+                <td><input type="number" name="" value="1"></td>
+                <td><input type="number" name="" value="1"></td> 
+                <td><input type="number" name="" value="1"></td>
+                <td><input type="number" name="" value="1"></td>
+                <td><input type="number" name="" value="1"></td> 
+                <td><input type="number" name="" value="1"></td>
+                <td><input type="number" name="" value="1"></td>
+              </tr>
+
+            </table>
+
           <button class="buttonStart">Start Game</button> 
+          <div>
+            
+          </div>
     </div>
     
   </div>
@@ -15,6 +90,5 @@ import "./assets/css/main.css";
 
 export default {
   name: "app",
- 
 };
 </script>
--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"math/rand"
+	"time"
+
 	"github.com/leaanthony/mewn"
 	"github.com/wailsapp/wails"
 )
+
+func numbers() {
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(a), func(i, j int) { a[i], a[j] = a[j], a[i] })
+
+}
 
 func main() {
 
@@ -18,6 +28,6 @@ func main() {
 		CSS:    css,
 		Colour: "#131313",
 	})
-
+	app.Bind(numbers)
 	app.Run()
 }
